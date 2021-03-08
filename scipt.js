0b3ceaf610fd6a96260279ed83d3083980fc7651
Farkas Sándor <sj.farkas64@gmail.com>
2021-03-08 15:01:48 +0100

    add base file structure

---
+++ scipt.js
@@ -0,0 +1,27 @@
+function colorClock() {
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+if(seconds <10) {
+  seconds = '0' + seconds;
+} 
+if(minutes < 10) {
+  minutes = '0' + minutes;
+}
+if(hours < 10) {
+  hours = '0' + hours;
+}
+var clockFace = hours + ':' + minutes + ':' + seconds;
+
+document.getElementById('clock').innerHTML = clockFace;
+
+setTimeout(function() {
+  colorClock();
+}, 1000);
+}
+
+colorClock();
+
+
